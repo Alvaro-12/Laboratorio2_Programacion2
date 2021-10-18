243e3561b7caa442b89b1309e6abd8d84cd36d96
davidcalles1 <cc0324032019@unab.edu.sv>
2021-10-18 11:12:01 -0600

    1

--- Laboratorio2/Dominio/persona.cs
+++ Laboratorio2/Dominio/persona.cs
@@ -7,15 +7,29 @@ using System.Threading.Tasks;
 
 namespace Laboratorio2.Dominio
 {
+    [Table ("Persona")]
+
     public class persona
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 
         public int IdPersona { get; set; }
+        [Display (Name = "NombrePersona")]
+        [Required (ErrorMessage = "Campo requerido ")]
+        [StringLength (50,MinimumLength =3,ErrorMessage = "El nombre de la persona tiene que estar entre 3 y 50 caracteres")]
+
         public  string NombrePersona { get; set;}
+        [Display (Name = "EdadPersona")]
+        [Required(ErrorMessage = "Campo requerido ")]
+        [Range (18,122, ErrorMessage = "Eres menor de edda no puedes entrar ")]
         public int EdadPersona { get; set; }
+        [Display (Name = "DescripcionPersona")]
+        [Required(ErrorMessage = "Campo requerido ")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Maximo  50 caracteres")]
         public string DescripcionPersona { get; set; }
+
+         
    
     }
 }
